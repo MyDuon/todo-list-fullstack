dd0b49d598d62edef11d7a597f0c1415013e5e42
MyDuon <100708123+MyDuon@users.noreply.github.com>
2025-08-25 14:16:57 +0200

    intialized frontend with vite

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,4 +1,10 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("hello world");
+import express from "express";
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
 //# sourceMappingURL=index.js.map
